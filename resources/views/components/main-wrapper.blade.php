f052f77949a3477ffc33eda2824774bc561301b3
tychoandreakos <elangidra15@gmail.com>
2020-12-05 16:26:30 +0700

    Fixed source in the topnav

--- resources/views/components/main-wrapper.blade.php
+++ resources/views/components/main-wrapper.blade.php
@@ -2,5 +2,6 @@
 <!-- Main wrapper - style you can find in pages.scss -->
 <!-- ============================================================== -->
 <div id="main-wrapper">
-
+    @include('components.topbar')
+{{--    @include('components.left-sidebar')--}}
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 
 @section('content')
-    <h3>Hello World</h3>
+{{--    @include('components.spinner')--}}
+    @include('components.main-wrapper')
 @endsection
