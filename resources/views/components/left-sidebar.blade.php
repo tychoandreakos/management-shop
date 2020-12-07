53fb0e7658a396df9d70518977a9d16435eda872
tychoandreakos <elangidra15@gmail.com>
2020-12-07 19:37:09 +0700

    Relayout child sidebar

--- resources/views/components/left-sidebar.blade.php
+++ resources/views/components/left-sidebar.blade.php
@@ -42,25 +42,18 @@
             <ul id="sidebarnav">
                 <li class="nav-devider"></li>
                 <li class="nav-small-cap">PERSONAL</li>
-                <li><a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i
+                <li><a class="" href="#" aria-expanded="false"><i
                             class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard <span
                                 class="label label-rouded label-themecolor pull-right">4</span></span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="index.html">Minimal </a></li>
-                        <li><a href="index2.html">Analytical</a></li>
-                        <li><a href="index3.html">Demographical</a></li>
-                        <li><a href="index4.html">Modern</a></li>
-                    </ul>
                 </li>
                 <li><a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i
-                            class="mdi mdi-bullseye"></i><span class="hide-menu">Apps</span></a>
+                            class="mdi mdi-bullseye"></i><span class="hide-menu">Master Management</span></a>
                     <ul aria-expanded="false" class="collapse">
-                        <li><a href="app-calendar.html">Calendar</a></li>
-                        <li><a href="app-chat.html">Chat app</a></li>
-                        <li><a href="app-ticket.html">Support Ticket</a></li>
-                        <li><a href="app-contact.html">Contact / Employee</a></li>
-                        <li><a href="app-contact2.html">Contact Grid</a></li>
-                        <li><a href="app-contact-detail.html">Contact Detail</a></li>
+                        <li><a href="{{route('customers.home')}}">Customers</a></li>
+                        <li><a href="{{ route('items.home')  }}">Items</a></li>
+                        <li><a href="{{ route('brands.home')  }}">Brands</a></li>
+                        <li><a href="{{ route('specifications.home')  }}">Specification Items</a></li>
+                        <li><a href="{{ route('categories.home')  }}">Categories</a></li>
                     </ul>
                 </li>
                 <li><a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i
