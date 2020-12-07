fa2a018b0b87f75f327e6cddcd0b2319f54997a3
tychoandreakos <elangidra15@gmail.com>
2020-12-07 20:05:45 +0700

    Finishing topbar

---
+++ resources/views/components/topbars/user.blade.php
@@ -0,0 +1,30 @@
+<ul class="navbar-nav my-lg-0">
+    <!-- ============================================================== -->
+    <!-- Profile -->
+    <!-- ============================================================== -->
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href=""
+           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img
+                src="{{ asset('assets/images/users/1.jpg')  }}" alt="user" class="profile-pic"/></a>
+        <div class="dropdown-menu dropdown-menu-right scale-up">
+            <ul class="dropdown-user">
+                <li>
+                    <div class="dw-user-box">
+                        <div class="u-img"><img src="{{ asset('assets/images/users/1.jpg')  }}" alt="user"></div>
+                        <div class="u-text">
+                            <h4>Steave Jobs</h4>
+                            <p class="text-muted">varun@gmail.com</p>
+                            <a href="pages-profile.html" class="btn btn-rounded btn-danger btn-sm mt-2">View
+                                Profile</a></div>
+                    </div>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"><i class="ti-user"></i> My Profile</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+            </ul>
+        </div>
+    </li>
+</ul>
