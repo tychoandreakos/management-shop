aceac2b8c4b83623d9e84dbc34d132b017d191c3
tychoandreakos <elangidra15@gmail.com>
2020-12-08 21:20:59 +0700

    multiselect

--- resources/views/components/item_transactions/multiselect.blade.php
+++ resources/views/components/item_transactions/multiselect.blade.php
@@ -17,16 +17,6 @@
 @push('css')
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css"/>
-    <style type="text/css">
-        .dropdown-toggle {
-            height: 40px;
-            width: 100% !important;
-            border: 1px solid #bfc3c3;
-            background: #fff;
-            color: #67757c;
-            font-size: .9rem !important;
-        }
-    </style>
 @endpush
 @push('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
