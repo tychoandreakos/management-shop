3b077191d3b7fecf6b7b42c10e28422f893bf19b
tychoandreakos <elangidra15@gmail.com>
2020-12-08 11:11:23 +0700

    Adding shipping provider to the DB

---
+++ database/migrations/2020_12_08_040647_create_ship_provider_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipProviderTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ship_provider_transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->unsignedBigInteger('ship_provider_id');
+            $table->string('ordering_number', 100);
+            $table->string('sending_status');
+
+            
+            $table->foreign('ship_provider_id')->references('id')->on('ship_providers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ship_provider_transactions');
+    }
+}
