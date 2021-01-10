5388494ff4c7ff1b7aa19a613f2feb879044e2d6
tychoandreakos <elangidra15@gmail.com>
2021-01-10 12:34:50 +0700

    Adding create new label feature

--- resources/views/components/customers/left-aside.blade.php
+++ resources/views/components/customers/left-aside.blade.php
@@ -4,11 +4,12 @@
         </li>
         <li class="divider"></li>
         @foreach($customerLabels as $customerLabel)
-            <li><a href="javascript:void(0)">{{ $customerLabel->name  }} <span>{{ $customerLabel->customer_label_transaction_count  }}</span></a></li>
+            <li><a href="javascript:void(0)">{{ $customerLabel->name  }}
+                    <span>{{ $customerLabel->customer_label_transaction_count  }}</span></a></li>
         @endforeach
 
         <li class="box-label mt-3"><a href="javascript:void(0)" data-toggle="modal"
-                                 data-target="#myModal" class="btn btn-info text-white">+ Create
+                                      data-target="#myModal" class="btn btn-info text-white">+ Create
                 New Label</a></li>
         <div id="myModal" class="modal fade in" tabindex="-1" role="dialog"
              aria-labelledby="myModalLabel" aria-hidden="true">
@@ -25,26 +26,13 @@
                             <div class="form-group">
                                 <label class="col-md-12">Name of Label</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control"
+                                    <input name="name" type="text" class="form-control"
                                            placeholder="type name"></div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-12">Select Number of people</label>
-                                <div class="col-md-12">
-                                    <select class="form-control">
-                                        <option>All Contacts</option>
-                                        <option>10</option>
-                                        <option>20</option>
-                                        <option>30</option>
-                                        <option>40</option>
-                                        <option>Custome</option>
-                                    </select>
-                                </div>
                             </div>
                         </from>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-info waves-effect"
+                        <button type="button" id="save-label" class="btn btn-info waves-effect"
                                 data-dismiss="modal">Save
                         </button>
                         <button type="button" class="btn btn-default waves-effect"
@@ -58,3 +46,35 @@
         </div>
     </ul>
 </div>
+
+
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            const url = "{{ route("customer-labels.store")  }}"
+            const redirect = "{{ route("customers.home")  }}"
+            $('#save-label').click(function () {
+                saveMe()
+            })
+
+            $('input[name="name"]').on('keypress', function (e) {
+                if (e.which === 13) saveMe()
+            })
+
+            // next
+            function validator() {
+            }
+
+            function saveMe() {
+                $.post(url, {
+                    _token: $('meta[name="_token"]').attr('content'),
+                    name: $('input[name="name"]').val()
+                }).done(function () {
+                    window.location.href = redirect
+                }).fail(function () {
+                    console.log('error')
+                })
+            }
+        })
+    </script>
+@endpush
