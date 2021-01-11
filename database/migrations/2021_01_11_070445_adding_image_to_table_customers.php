e45762700fb826b3744ae7264fc943da8f373214
tychoandreakos <elangidra15@gmail.com>
2021-01-11 14:06:08 +0700

    Finishing adding new coloumn

--- database/migrations/2021_01_11_070445_adding_image_to_table_customers.php
+++ database/migrations/2021_01_11_070445_adding_image_to_table_customers.php
@@ -14,7 +14,7 @@ class AddingImageToTableCustomers extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->after('suggestion');
         });
     }
 
