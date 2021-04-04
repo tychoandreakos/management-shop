7d8f2ccd8c6292d6f25d3561353cfce69cbfada2
tychoandreakos <elangindra15@gmail.com>
2021-04-04 05:54:32 +0000

    Change the graph yearli earning to month earning in the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,9 +17,41 @@ class DashboardController extends Controller
                 ->first(),
             "orderStats" => $this->orderStats(),
             "titleHeader" => "Dashboard",
+            "graphStats" => collect([
+                [
+                    "period" => '2010',
+                    "Customers" => 50,
+                    "Orderings" => 150,
+                ],
+                [
+                    "period" => '2011',
+                    "Customers" => 200,
+                    "Orderings" => 100,
+                ],
+                [
+                    "period" => '2012',
+                    "Customers" => 240,
+                    "Orderings" => 120,
+                ],
+                [
+                    "period" => '2013',
+                    "Customers" => 200,
+                    "Orderings" => 212,
+                ],
+                [
+                    "period" => '2014',
+                    "Customers" => 42,
+                    "Orderings" => 302,
+                ],
+                [
+                    "period" => '2015',
+                    "Customers" => 50,
+                    "Orderings" => 122,
+                ],
+            ])->toJson(),
         ];
-				
-				//return response()->json($data);
+
+       // return response()->json($data);
         return view("dashboard.home")->with($data);
     }
 
--- resources/views/components/dashboards/graphic.blade.php
+++ resources/views/components/dashboards/graphic.blade.php
@@ -32,46 +32,10 @@
             // ==============================================================
             Morris.Area({
                 element: 'morris-area-chart2',
-                data: [{
-                    period: '2010',
-                    iMac: 0,
-                    iPhone: 0,
-
-                }, {
-                    period: '2011',
-                    iMac: 130,
-                    iPhone: 100,
-
-                }, {
-                    period: '2012',
-                    iMac: 30,
-                    iPhone: 60,
-
-                }, {
-                    period: '2013',
-                    iMac: 30,
-                    iPhone: 200,
-
-                }, {
-                    period: '2014',
-                    iMac: 200,
-                    iPhone: 150,
-
-                }, {
-                    period: '2015',
-                    iMac: 105,
-                    iPhone: 90,
-
-                },
-                    {
-                        period: '2016',
-                        iMac: 250,
-                        iPhone: 150,
-
-                    }],
+                data: JSON.parse(@json($graphStats)),
                 xkey: 'period',
-                ykeys: ['iMac', 'iPhone'],
-                labels: ['iMac', 'iPhone'],
+                ykeys: ['Customers', 'Orderings'],
+                labels: ['Customers', 'Orderings'],
                 pointSize: 0,
                 fillOpacity: 0.4,
                 pointStrokeColors: ['#b4becb', '#01c0c8'],
