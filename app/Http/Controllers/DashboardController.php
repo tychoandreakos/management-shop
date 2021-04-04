3c1a66efab1e57ad9cba6a49941b4fbfb28c9977
tychoandreakos <elangindra15@gmail.com>
2021-04-04 05:05:14 +0000

    Fixed some error

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,28 +5,40 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\Models\ShipProviderTransaction;
 
-
 class DashboardController extends Controller
 {
     public function index()
     {
         $data = [
-            'breadCrumbs' => "Dashboard",
-            'productOverviews' => $this->product_overview(),
-            'latestProduct' => Item::with('itemImageTransaction.itemImage')->latest()->first(),
-            'orderStats' => $this->orderStats(),
-	    'titleHeader' => 'Dashboard'
+            "breadCrumbs" => "Dashboard",
+            "productOverviews" => $this->product_overview(),
+            "latestProduct" => Item::with("itemImageTransaction.itemImage")
+                ->latest()
+                ->first(),
+            "orderStats" => $this->orderStats(),
+            "titleHeader" => "Dashboard",
         ];
-
-        return view('dashboard.home')->with($data);
+				
+				//return response()->json($data);
+        return view("dashboard.home")->with($data);
     }
 
     private function orderStats(): array
     {
         $data = [
-            'pending' => ShipProviderTransaction::where('sending_status', 'pending')->get()->count(),
-            'delivered' => ShipProviderTransaction::where('sending_status', 'send')->get()->count(),
-            'orders' => ShipProviderTransaction::all()->count()
+            "pending" => ShipProviderTransaction::where(
+                "sending_status",
+                "pending"
+            )
+                ->get()
+                ->count(),
+            "delivered" => ShipProviderTransaction::where(
+                "sending_status",
+                "send"
+            )
+                ->get()
+                ->count(),
+            "orders" => ShipProviderTransaction::all()->count(),
         ];
 
         return $data;
@@ -34,6 +46,9 @@ class DashboardController extends Controller
 
     private function product_overview()
     {
-        return ShipProviderTransaction::with('customer', 'item.itemImageTransaction.itemImage')->get();
+        return ShipProviderTransaction::with(
+            "customer",
+            "item.itemImageTransaction.itemImage"
+        )->get();
     }
 }
